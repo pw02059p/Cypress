bd188432afd4d6f8143541282f5b4f81c76d672e
pw02059p <pw02059p1@hotmail.com>
2022-04-13 14:26:20 -0300

    Visit/Login

--- cypress/integration/1-Projeto/Mevo.spec.js
+++ cypress/integration/1-Projeto/Mevo.spec.js
@@ -2,9 +2,20 @@
 
 
 describe('Mevo', () => {
-
-    it('Validacao', () => {
-        
-    });
+    before(() => {
+       cy.visit('https://farmacias-staging.mevosaude.com.br/')
+    })
     
+    it('Validacao', () => {
+        cy.get('[data-testid="code-access"] > .shadow-layer-0')
+        .click() 
+        cy.get('[data-testid="token-input"]')
+        .type('NXT6WYD')
+        cy.get('#pin-input-1-0').type('0')
+        cy.get('#pin-input-1-1').type('9')
+        cy.get('#pin-input-1-2').type('5')
+        cy.get('#pin-input-1-3').type('7')
+        cy.get('.css-1xc6u01 > .sc-jSgupP').click()
+    });
+        
 });
