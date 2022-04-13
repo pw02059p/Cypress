49e331fb6fa8cfc221a94ca99fd6723642192e77
pw02059p <pw02059p1@hotmail.com>
2022-04-13 17:11:59 -0300

    Case Mevo Completo
    Nao possui itens 3/4 por falta de necessidade dos mesmo.

--- cypress/integration/1-Projeto/Mevo.spec.js
+++ cypress/integration/1-Projeto/Mevo.spec.js
@@ -1,21 +1,31 @@
 /// <reference types="cypress" />
 
-
-describe('Mevo', () => {
+describe('Mevo-Validacao', () => {
     before(() => {
-       cy.visit('https://farmacias-staging.mevosaude.com.br/')
+        cy.visit('https://farmacias-staging.mevosaude.com.br/')
     })
-    
-    it('Validacao', () => {
+
+    it('Login', () => {
         cy.get('[data-testid="code-access"] > .shadow-layer-0')
-        .click() 
+            .click()
         cy.get('[data-testid="token-input"]')
-        .type('NXT6WYD')
+            .type('NXT6WYD')
         cy.get('#pin-input-1-0').type('0')
         cy.get('#pin-input-1-1').type('9')
         cy.get('#pin-input-1-2').type('5')
         cy.get('#pin-input-1-3').type('7')
-        cy.get('.css-1xc6u01 > .sc-jSgupP').click()
-    });
-        
-});
+        cy.get('.shadow-layer-1').children().contains('Consultar receita').click()
+    })
+
+    it('Validar-Mensagem', () => {
+        cy.get('.glow-focus')
+            .contains('Dispensar receita').click()
+        cy.get('#chakra-modal-36')
+            .contains('Nenhum medicamento selecionado')
+        cy.get('#chakra-modal-36')
+            .contains('Selecione o(s) medicamento(s) para seguir com a dispensação')
+        cy.get('#chakra-modal-36')
+            .children()
+            .contains('Entendi').click()
+    })
+})
